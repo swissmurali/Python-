656802c330840e37b579bc3545a7e1653afed89c
Murali G <muralig@hayagriv.com>
2011-05-03 20:15:22 +0530

    Signed-off-by: Murali G <muralig@hayagriv.com>

---
+++ exc4.py
@@ -0,0 +1,18 @@
+my_name = 'Murali.G'
+my_age = 26
+my_weight = 65 # kgs
+my_height = 170 # cms
+my_eyes = 'white'
+my_teeth = 'white'
+my_hair = 'black'
+
+
+print "My name is %s." % my_name
+print "My age is %d." % my_age
+print "My weight is %d." % my_weight
+print "My height is %d." % my_height
+print "My eye color is %s, my hair color is %s and my teeth color is %s." % (my_eyes, my_hair,my_teeth)
+print "if add %d, %d and %d total %d." % (my_age, my_weight, my_height, my_age + my_weight + my_height)
+
+
+
