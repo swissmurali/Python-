9dd231eec0c20e62a16e81171578e46d0104dc9d
muralig <muralig@hayagriv.com>
2011-05-06 12:14:29 +0530

    exercise

---
+++ exercise001.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+print "Hello Agsin"
+print " I like typing this."
+print "This is fun"
+print 'Yah! Printing.'
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this.'
