90d14ab8fa8dc57ea666c4eba01b7c246cbf1b40
muralig <muralig@hayagriv.com>
2011-05-12 12:26:20 +0530

    New Files

---
+++ exc10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm \"tabbed\" in." # has escape double quotes inside string
+persian_cat = "I'm split\non a line." # '/n' for new line
+backslash_cat = "I'm \\ a \\ cat." # '//' for printing '/'
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+''' # ''' or """ for printing multiple string
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
---
+++ exc9.py
@@ -0,0 +1,11 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+print """
+Theres something going on here.
+With the three double-quotes.
+Well be able to type as much as we like.
